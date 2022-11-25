a215adeff8ca8df931ce4251c2b830064c1e24f4
giovani <giovaniszg@gmail.com>
2022-11-24 23:02:57 -0500

    tercer commit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,7 +10,7 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   Future<String> getFullName() async {
-    return "Jazmin Lucero";
+    return "giovani";
   }
 
   showDialogForm() {
